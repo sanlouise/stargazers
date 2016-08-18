0f04d0104125601e7141cfb072effb38db948cab
kelsonic <kgadams93@gmail.com>
2016-08-18 17:01:49 +0200

    Add video posts migration and controllers for posts

--- db/migrate/20160818095804_create_video_posts.rb
+++ db/migrate/20160818095804_create_video_posts.rb
@@ -2,6 +2,7 @@ class CreateVideoPosts < ActiveRecord::Migration[5.0]
   def change
     create_table :video_posts do |t|
     	t.integer :user_id
+      t.integer :category_id
       t.string :video
       t.string :location
       t.string :description
