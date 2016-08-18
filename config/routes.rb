ddc4e86265e207058b768b4b289559a7bfe2ce34
sanlouise <sandra.adamshallie@gmail.com>
2016-08-18 14:37:06 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :astronomers
+
   root 'pages#home'
+
+  get '/about', to: 'pages#about'
+  
+  resources :profiles, only: [:show, :edit, :update]
+
 end
