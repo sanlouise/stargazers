cdc666f19256666b82a14233b85ccb4bfef69347
kelsonic <kgadams93@gmail.com>
2016-08-21 22:55:24 +0200

    Remove image/video posts in favor of general posts

--- app/models/image_post.rb
+++
@@ -1,7 +0,0 @@
-class ImagePost < ActiveRecord::Base
-	belongs_to :user
-
-	validates :image, presence: true
-	validates :location, presence: true
-	validates :description, presence: true, length: { maximum: 300 }
-end
